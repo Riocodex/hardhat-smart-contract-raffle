bf17064e2503e13bfa96df0de51e1cf4f8fc1413
Riocodex <onwukachibike@gmail.com>
2022-09-01 05:45:00 +0100

    created the basic smartcontract
    added the chainlink smartcontract and created the primary functions and edited the function for randomnesss

--- contracts/raffle.sol
+++ contracts/raffle.sol
@@ -1,6 +1,40 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.7;
-contract Raffle{
-    
+
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+error Raffle__NotEnoughETHEntered();
+contract Raffle is VRFConsumerBaseV2{
+    //state variables
+    uint256 private immutable i_entranceFee;
+    address payable [] private s_players;
+
+    //Events
+    event RaffleEnter(address indexed player);
+
+    constructor(address vrfCoordinatorV2,uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2){
+        i_entranceFee = entranceFee;
+    } 
+
+    function enterRaffle()public payable{
+        if(msg.value < i_entranceFee){
+            revert Raffle__NotEnoughETHEntered();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
+     function requestRandomWinner() external{
+
+     }
+     function fufillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal 
+        override
+     {}
+    /*View / Pure functions */
+    function getEntranceFee() public view returns (uint256){
+        return i_entranceFee;
+    }
+    function getPlayer(uint256 index)public view returns(address){
+        return s_players[index];
+    }
 }
