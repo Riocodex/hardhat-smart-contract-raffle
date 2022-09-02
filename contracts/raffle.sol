143cae08dee69df35783726e336088c6db839a6b
Riocodex <onwukachibike@gmail.com>
2022-09-02 22:34:20 +0100

    added the upkeep functions
    i used chainlink libraries upkeep function fo rmaking the smart contract fully automated i brought in the upkeep function and made certain conditions that would be made to deploy the smart contract automatically

--- contracts/raffle.sol
+++ contracts/raffle.sol
@@ -1,13 +1,34 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.7;
-
+//for randomness
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+//for upkeepness(that is being automated and running by itself)
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 
 error Raffle__NotEnoughETHEntered();
 error Raffle__TransferFailed();
-contract Raffle is VRFConsumerBaseV2{
+error Raffle_NotOpen();
+error Raffle__UpKeepNotNeeded(
+    uint256 currentBalance , 
+    uint256 numPlayers , 
+    uint256 raffleState
+);
+
+/**@title A sample Raffle or lottery contract
+ * @author Onwuka Rosario
+ * @notice this contract is for creating an untamperable automated decentralized smart contract
+ * @dev this implements chainlink VRF v2 and Chainlink keepers
+ * 
+ */
+
+contract Raffle is VRFConsumerBaseV2 , KeeperCompatibleInterface{
+    //Type declarations
+    enum RaffleState{
+        OPEN,
+        CALCULATING 
+    }
     //state variables
     uint256 private immutable i_entranceFee;
     address payable [] private s_players;
@@ -20,6 +41,9 @@ contract Raffle is VRFConsumerBaseV2{
 
     //lottery variables
     address private s_recentWinner;
+    RaffleState private s_raffleState;
+    uint256 private s_lastTimeStamp;
+    uint256 private immutable i_interval;
 
     //Events
     event RaffleEnter(address indexed player);
@@ -31,23 +55,64 @@ contract Raffle is VRFConsumerBaseV2{
         uint256 entranceFee ,
          bytes32 gasLane,
          uint64 subscriptionId,
-         uint32 callbackGasLimit
+         uint32 callbackGasLimit , 
+         uint256 interval
         ) VRFConsumerBaseV2(vrfCoordinatorV2){
             i_entranceFee = entranceFee;
             i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
             i_gasLane = gasLane;
             i_subscriptionId = subscriptionId;
             i_callbackGasLimit = callbackGasLimit;
+            s_raffleState = RaffleState.OPEN;
+            s_lastTimeStamp = block.timestamp;
+            i_interval = interval;
     } 
 
     function enterRaffle()public payable{
         if(msg.value < i_entranceFee){
             revert Raffle__NotEnoughETHEntered();
         }
+        //if raffle isnt open
+        if(s_raffleState != RaffleState.OPEN){
+            revert Raffle__NotOpen();
+        }
         s_players.push(payable(msg.sender));
         emit RaffleEnter(msg.sender);
     }
-     function requestRandomWinner() external{
+    /**
+     * @dev this is the function the chain keeper nodes call
+     * they look for the 'upKeepNeeded' to return true
+     * when it returns true thats when the random number changes
+     * the following should be true in order to return true:
+     * 1)Our time interval should have passed
+     * 2)The lottery should have atleast 1 player, and have some ETH
+     * 3)our subscription is funded with LINK
+     * 4)the lottery should be in an open state.
+     */
+    function checkUpKeep(bytes calldata /*checkdata*/)public override returns(bool upKeepNeeded , bytes memory /*performData*/{
+        //to chech whether the lottery is open
+        bool isOpen = (RaffleState.OPEN == s_raffleState);
+        //to check whether the time interval has passed
+        bool timePassed  = ((block.timestamp - s_lastTimeStamp) > i_interval);
+        //to check if the lottery has atleast one player
+        bool hasPlayers = (s_players.length > 0);
+        //to check whether the smartcontract has any balance
+        bool hasBalance  = address(this).balance > 0;
+        //to rap up all the bools and put it in the inherited chainlink function to retur one last statement
+        bool upKeepNeeded = (isOpen && timePassed && hasPlayers && hasBalance);
+    }
+     function performUpKeep(bytes calldata /*performdata*/) external override{
+        (bool upKeepNeeded , ) = checkUpKeep("");
+        //if upkeep is not true
+        if(!upKeepNeeded) {
+            revert Raffle__UpKeepNotNeeded(
+                //this variables are to show the user the cause of the problem..whether there is no money in the smartcontract,or there are no players or the lottery isnt open yet
+                address(this).balance,
+                s_player.length,
+                uint256(s_raffleState)
+            );
+        }
+        s_raffleState = Raffle.CALCULATING;
         uint256 requestId = i_vrfCoordinator.requestRandomWords(
              i_gasLane,
              i_subscriptionId,
@@ -65,6 +130,12 @@ contract Raffle is VRFConsumerBaseV2{
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner]; 
         s_recentWinner = recentWinner;
+        //when there is a winner the lottery is open
+        s_raffleState = RaffleState.OPEN;
+        //when a winner has been picked array of users refresh
+        s_players = new address payable[](0);
+        //when theres a winner the timestamp resets
+        s_lastTimeStamp = block.timestamp;
         (bool success , ) = recentWinner.call{value: address(this).balance}("");
         //require(success)
         if(!success ){
@@ -82,4 +153,5 @@ contract Raffle is VRFConsumerBaseV2{
     function getRecentWinner() public view returns(address){
         return s_recentWinner;
     }
+    function getRaffleState() public
 }
