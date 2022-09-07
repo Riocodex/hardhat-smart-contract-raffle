bf58e66e2c403a3c6a45735c20d1fef523a2b8b1
Riocodex <onwukachibike@gmail.com>
2022-09-07 04:44:15 +0100

    finished adding every parameter in the constructor

--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,21 +1,32 @@
 const { network } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
+
+const VRF_SUB_FUND_AMOUNT = ethers.utils.parseEther("30");
+
 module.exports = async function ({ getNamedAccounts , deployments}) {
     const { deploy , log } = deployments;
     const {deployer} = await getNamedAccounts();
     const chainId = network.config.chainId;
-    let vrfCoordinatorV2Address;
+    let vrfCoordinatorV2Address , subscriptionId
 
     if (developmentChains.includes(network.name)){
         const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
         vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address;
+        const transactionResponse = await vrfCoordinatorV2Mock.createSubscription();
+        const transactionReceipt = await transactionResponse.wait(1);
+        subscriptionId = transactionReceipt.events[0].args.subId;
+        await vrfCoordinatorV2Mock.fundSubscription(subscriptionId , VRF_SUB_FUND_AMOUNT)
     }else{
         vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
+        subscriptionId = networkConfig[chainId]["subscriptionId"];
     }
 
     const entranceFee = networkConfig[chainId]["entranceFee"];
     const gasLane = networkConfig[chainId]["gasLane"];
-    const args = [vrfCoordinatorV2Address , entranceFee , gasLane];
+    const callbackGasLimit = networkConfig[chainId]["callbackGasLimit"];
+    const interval = networkConfig[chainId]["interval"];
+
+    const args = [vrfCoordinatorV2Address , entranceFee , gasLane , callbackGasLimit , interval];
     const raffle = await deploy ("Raffle" , {
         from:deployer ,
         args:args,
--- helper-hardhat.-config.js
+++ helper-hardhat.-config.js
@@ -3,11 +3,17 @@ const networkConfig = {
         name: 'rinkeby' , 
         vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab" ,
         entranceFee : ethers.utils.parseEther("0.01"), 
-        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc"
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc" , 
+        subscriptionId : "0" , 
+        callbackGasLimit : "500000",
+        interval: "30" , 
     },
     31337: {
         name: "hardhat" , 
         entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc" ,
+        callbackGasLimit: "500000",
+        interval: "30" , 
     }
 }
 
