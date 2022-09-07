4699d466a4408ea09f475ad49496ebecaabad1bb
Riocodex <onwukachibike@gmail.com>
2022-09-07 04:01:29 +0100

    edited the javascript deploy files

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,7 +1,8 @@
 const { developmentChains } = require("../helper-hardhat-config")
 
-const BASE_FEE = ethers.utils.parseEther("0.25")
-
+const BASE_FEE = ethers.utils.parseEther("0.25")//0.25 is the premium , it cost 0.25 link per request
+const GAS_PRICE_LINK =1e9 //calculated value based on the gas price of the chain
+const args = [BASE_FEE , GAS_PRICE_LINK]
 module.exports = async function ({ getNamedAccounts , deployments}) {
     const { deploy , log } = deployments
     const { deployer } = await getNamedAccounts()
@@ -11,7 +12,12 @@ module.exports = async function ({ getNamedAccounts , deployments}) {
         log("local network detected! Deploying mocks...")
         //deploy a mock vrfCoordinator
         await deploy("VRFCoordinatorV2Mock" , {
-
+            from: deployer , 
+            log: true ,
+            args: args, 
         })
+        log("Mocks Deployed!")
+        log("--------------------------------")
     }
 }
+module.exports.tags = ["all" , "mocks"];
--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,10 +1,22 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
 module.exports = async function ({ getNamedAccounts , deployments}) {
     const { deploy , log } = deployments;
     const {deployer} = await getNamedAccounts();
+    const chainId = network.config.chainId;
+    let vrfCoordinatorV2Address;
 
+    if (developmentChains.includes(network.name)){
+        const VRFCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+        vrfCoordinatorV2Address = VRFCoordinatorV2Mock.address;
+    }else{
+        vrfCoordinatorV2Address = networkConfig[chainId]["vrfCoordinatorV2"];
+    }
+
+    const args = []
     const raffle = await deploy ("Raffle" , {
         from:deployer ,
-        args:[],
+        args:args,
         log:true,
         waitConfirmations: network.config.blockConfirmations || 1,
     })
